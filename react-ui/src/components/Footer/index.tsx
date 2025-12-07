6f7ceb21ae49ca48611ae3a8521be630be405fd2
u679c <22016021001@stu.nsu.edu.cn>
2025-12-07 17:16:56 +0800

    Initial commit

---
+++ react-ui/src/components/Footer/index.tsx
@@ -0,0 +1,36 @@
+import { GithubOutlined } from '@ant-design/icons';
+import { DefaultFooter } from '@ant-design/pro-components';
+import React from 'react';
+
+const Footer: React.FC = () => {
+  return (
+    <></>
+    // <DefaultFooter
+    //   style={{
+    //     background: 'none',
+    //   }}
+    //   links={[
+    //     {
+    //       key: 'CNTS Pro',
+    //       title: 'CNTS Pro',
+    //       href: 'https://pro.ant.design',
+    //       blankTarget: true,
+    //     },
+    //     {
+    //       key: 'github',
+    //       title: <GithubOutlined />,
+    //       href: 'https://github.com/ant-design/ant-design-pro',
+    //       blankTarget: true,
+    //     },
+    //     {
+    //       key: 'CNTS',
+    //       title: 'CNTS',
+    //       href: 'https://ant.design',
+    //       blankTarget: true,
+    //     },
+    //   ]}
+    // />
+  );
+};
+
+export default Footer;
